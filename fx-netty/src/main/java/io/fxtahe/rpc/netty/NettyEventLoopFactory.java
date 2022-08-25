20f99ce3c49efed8a9d2d8e0837f36338eab3f2d
fxtahe <270295484@qq.com>
2022-08-25 21:55:29 +0800

    netty init

---
+++ fx-netty/src/main/java/io/fxtahe/rpc/netty/NettyEventLoopFactory.java
@@ -0,0 +1,43 @@
+package io.fxtahe.rpc.netty;
+
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.epoll.Epoll;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.epoll.EpollServerSocketChannel;
+import io.netty.channel.epoll.EpollSocketChannel;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.ServerSocketChannel;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @author fxtahe
+ * @since 2022/8/25 15:38
+ */
+public class NettyEventLoopFactory {
+
+
+    private static final String OS_NAME = "os.name";
+
+    private static final String OS_LINUX_PREFIX = "linux";
+
+
+    public static EventLoopGroup buildEventLoopGroup(int threads){
+        return shouldEpoll() ? new EpollEventLoopGroup(threads) : new NioEventLoopGroup(threads);
+    }
+
+    public static Class<? extends SocketChannel> socketChannelClass() {
+        return shouldEpoll() ? EpollSocketChannel.class : NioSocketChannel.class;
+    }
+
+    public static Class<? extends ServerSocketChannel> serverSocketChannelClass() {
+        return shouldEpoll() ? EpollServerSocketChannel.class : NioServerSocketChannel.class;
+    }
+
+    public static boolean shouldEpoll(){
+        String osName = System.getProperty(OS_NAME);
+        return osName.toLowerCase().contains(OS_LINUX_PREFIX) && Epoll.isAvailable();
+    }
+
+}
